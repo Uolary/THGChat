db9fe6e6b19fd4cb6af7a3e6368503ee885f2b8f
Ivan Nasanovich <uolary@gmail.com>
2021-07-04 01:39:58 +0300

    Add Title component

--- client/src/components/Authorization/index.jsx
+++ client/src/components/Authorization/index.jsx
@@ -4,12 +4,14 @@ import { SignUp } from './SignUp';
 import { useState } from 'react';
 import { auth } from '../../const/components';
 import { SignIn } from './SignIn';
+import { Title } from '../Title';
 
 export const Authorization = () => {
   const [active, setActive] = useState();
 
   return (
     <div className='wrap'>
+      <Title />
       <div className={styles.wrapAuth}>
         {
           active === auth.signIn && <SignIn />
