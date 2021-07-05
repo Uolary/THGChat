967f069b75ad941f5e66c5a82e262c70affec0ff
Ivan Nasanovich <uolary@gmail.com>
2021-07-05 18:34:55 +0300

    Add form validation in SignUp.jsx

--- client/src/components/Authorization/SignUp.jsx
+++ client/src/components/Authorization/SignUp.jsx
@@ -2,53 +2,99 @@ import { useCallback, useState } from 'react';
 import styles from './styles.module.scss';
 
 const initialRegisterInfo = {
-  nickname: '',
-  password: '',
-  repeatedPassword: '',
-  bio: '',
+  nickname: {
+    value: '',
+    isValid: true,
+  },
+  password: {
+    value: '',
+    isValid: true,
+  },
+  repeatedPassword: {
+    value: '',
+    isValid: true,
+  },
+  bio: {
+    value: '',
+    isValid: true,
+  },
 };
 
 export const SignUp = () => {
   const [userInfo, setUserInfo] = useState(initialRegisterInfo);
 
+  const setClassName = useCallback((field) => {
+    return !userInfo[field].isValid ? styles.invalid : ''
+  }, [userInfo]);
+
   const changeInputHandler = useCallback((event) => {
     setUserInfo((prevInfo) => ({
       ...prevInfo,
-      [event.target.id]: event.target.value,
+      [event.target.id]: {
+        value: event.target.value,
+        isValid: true,
+      },
     }))
+  }, [userInfo]);
+
+  const changeValidUserField = (field) => {
+    setUserInfo((prevInfo) => ({
+      ...prevInfo,
+      [field]: {
+        ...prevInfo[field],
+        isValid: false,
+      },
+    }))
+  };
+
+  const submitHandler = useCallback((e) => {
+    e.preventDefault();
+
+    if (!userInfo.nickname.length || !userInfo.nickname.trim()) {
+      changeValidUserField('nickname');
+    }
+
+    if (userInfo.password !== userInfo.repeatedPassword) {
+      changeValidUserField('password');
+      changeValidUserField('repeatedPassword');
+    }
   }, [userInfo]);
 
   return (
     <>
       <h2 className={styles.title}>Registration</h2>
-      <form className={styles.form} id='form-registration'>
+      <form
+        className={styles.form}
+        id='form-registration'
+        onSubmit={submitHandler}
+      >
         <label>
-          <label htmlFor='nickname'>Nickname</label>
+          <label htmlFor='nickname' className={setClassName('nickname')}>Nickname</label>
           <span className={styles.symbol}>{`>`}</span>
           <input
             type='text'
             id='nickname'
-            value={userInfo.nickname}
+            value={userInfo.nickname.value}
             onChange={changeInputHandler}
           />
         </label>
         <label>
-          <label htmlFor='password'>Password</label>
+          <label htmlFor='password' className={setClassName('password')}>Password</label>
           <span className={styles.symbol}>{`>`}</span>
           <input
             type='password'
             id='password'
-            value={userInfo.password}
+            value={userInfo.password.value}
             onChange={changeInputHandler}
           />
         </label>
         <label>
-          <label htmlFor='repeatedPassword'>Repeat password</label>
+          <label htmlFor='repeatedPassword' className={setClassName('repeatedPassword')}>Repeat password</label>
           <span className={styles.symbol}>{`>`}</span>
           <input
             type='password'
             id='repeatedPassword'
-            value={userInfo.repeatedPassword}
+            value={userInfo.repeatedPassword.value}
             onChange={changeInputHandler}
           />
         </label>
@@ -59,13 +105,14 @@ export const SignUp = () => {
             <input
               type='text'
               id='bio'
-              value={userInfo.bio}
+              value={userInfo.bio.value}
               onChange={changeInputHandler}
             />
           </span>
         </label>
         <button
           className={styles.btn}
+          type='submit'
         >
           {`Sign up --->`}
         </button>
