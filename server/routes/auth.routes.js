2c9d21a9bb34b3e59601e30edcf863f337648cf6
Ivan Nasanovich <uolary@gmail.com>
2021-07-05 23:17:11 +0300

    Add auth routes

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,22 @@
+const verifySignUp = require('../middlewares/verifySignUp');
+const controller = require('../controllers/auth.controller');
+
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept',
+    );
+    next();
+  });
+
+  app.post(
+    '/api/auth/signup',
+    [
+      verifySignUp,
+    ],
+    controller.signup
+  );
+
+  app.post('/api/auth/signin', controller.signin);
+};
